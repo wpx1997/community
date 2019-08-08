e7d7696fec6601f003ddb050595f73591f9390df
wpx1997 <896590620@qq.com>
2019-08-08 20:25:47 +0800

    实现持久化登录状态

--- src/main/java/life/wpx1997/community/controller/AuthorizeController.java
+++ src/main/java/life/wpx1997/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -32,7 +34,7 @@ public class AuthorizeController {
     private UserMapper userMapper;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state, HttpServletRequest request){
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state, HttpServletRequest request, HttpServletResponse response){
         AccessokenDTO accessokenDTO = new AccessokenDTO();
         accessokenDTO.setClient_id(clientId);
         accessokenDTO.setClient_secret(clientSecret);
@@ -44,14 +46,16 @@ public class AuthorizeController {
 
         if(githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
-            userMapper.insert(user);
+
             //登录成功，写cookie和session
-            request.getSession().setAttribute("user",githubUser);
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else {
             //登录失败，重新登录
